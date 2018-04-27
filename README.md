72399e7e8a25cf0a71bf7bb02da0d513834b3b77
Andrei Nemes <simply.andrei@gmail.com>
2018-04-27 23:15:19 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # soft-scroll
-Scroll elements smoothly into view using CSS.
+
+## Introduction
+
+**soft-scroll** is a small library for animating scrollable viewports using CSS.
+
+## Installation
+
+```
+npm install soft-scroll
+```
+
+Alternatively, just download and include [soft-scroll.js](./soft-scroll.js)
+
+```html
+<script src="soft-scroll.js"></script>
+```
+
+## Usage
+
+**soft-scroll** needs a wrapper element within the scrollable container to work:
+
+```
+container
+└── wrapper
+    ├── element 1
+    ├── element 2
+    ├── element 3
+    └── ..element n
+```
+
+Using this structure, **soft-scroll** only needs the duration and element to be scrolled into view:
+
+```javascript
+softScroll(500, document.getElementById('target'));
+```
+
+The target element may be omitted, in which case **soft-scroll** will return a function that accepts it:
+
+```javascript
+const quickScroll = softScroll(500);
+
+quickScroll(document.getElementById('target'));
+```
+
+This is especially is useful for binding event listeners:
+
+```javascript
+document
+  .getElementById('target')
+  .addEventListener('click', softScroll(500));
+```
+
+In the case where an event is passed in, **soft-scroll** will detect and use the `target` property.
+
+## Limitations
+
+**soft-scroll** currently only works with horizontal scrolling.
+
+## License
+
+[MIT](LICENSE)
+
+
---
+++ soft-scroll.js
@@ -0,0 +1,52 @@
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define([], factory);
+  }
+  else if (typeof module === 'object' && module.exports) {
+    module.exports = factory();
+  }
+  else {
+    root.flipScroll = factory();
+  }
+}(typeof self !== 'undefined' ? self : this, function () {
+  var scrollIntoView = function (duration, element) {
+    var target;
+
+    if (element instanceof Node) {
+      target = element;
+    } else if (element instanceof Event) {
+      target = element.target;
+    }
+
+    var wrapper = target.parentNode;
+    var parent = wrapper.parentNode;
+
+    var parentBox = parent.getBoundingClientRect();
+    var targetBox = target.getBoundingClientRect();
+
+    var distance = targetBox.x - parentBox.x;
+
+    parent.scrollLeft = parent.scrollLeft + distance;
+
+    wrapper.style.transition = '';
+    wrapper.style.transform = 'translateX(' + distance + 'px)';
+
+    setTimeout(function () {
+      wrapper.style.transition = 'transform ' + duration + 'ms ease-out';
+      wrapper.style.transform = 'translateX(0)';
+    }, 60);
+  };
+
+  return function () {
+    switch (arguments.length) {
+      case 1:
+        var duration_1 = arguments[0];
+        return function (element) { return scrollIntoView(duration_1, element); };
+      case 2:
+        var duration_2 = arguments[0];
+        return scrollIntoView(duration_2, arguments[1]);
+      default:
+        throw new Error('scrollIntoView needs at least 1 argument; none was given.')
+    }
+  };
+}));
